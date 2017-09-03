48e9c4399fc0acaf8014b93b8a5b2c5921eba0b0
SkySailing <SkySailing@users.noreply.github.com>
2017-09-04 00:49:17 +0800

    Update 待定思考496. Next Greater Element I.cpp

--- 待定思考496. Next Greater Element I.cpp
+++ 待定思考496. Next Greater Element I.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& findNums, vector<int>& nums) {
         stack<int> s;       
-        unordered_map<int, int> m;
-        for (int n : nums) {                    //for (int i =0; i <= nums.length; i++) {}
+        unordered_map<int, int> m;   // 定义map，键 值都 int型
+        for (int n : nums) {   //元素类型t 元素变量x : 遍历对象obj)  //for (int i =0; i <= nums.length; i++) {}
             
-            while (s.size() && s.top() < n) {     //第一个数不用比较，直接是到了s.push(n);
-                m[s.top()] = n;                     // 这里没有看懂，自学stack和哈希表再回来解释2017.8.26
+            while (s.size() && s.top() < n) {   //第一个数不用比较，直接是到了s.push(n);
+                m[s.top()] = n;            // 在map中寻找键 为s.top的元素，如果没有，则插入一个新的键值对《s.top,n》
                 s.pop();
             }
-            s.push(n);  // 把
-        }
+            s.push(n);    // 把数字n放到堆栈的开头
+        }                 //上面的公式是为了匹配出每个数组的对应的最大值          
         vector<int> ans;
-        for (int n : findNums) ans.push_back(m.count(n) ? m[n] : -1);
-        return ans;
+        for (int n : findNums)  ans.push_back(m.count(n) ? m[n] : -1);   //m.count表示m中n键出现的次数，返回值只能是0和1
+        return ans;                       //在ans的最后一个向量后插入一个元素，其值为()
     }
 };
 
