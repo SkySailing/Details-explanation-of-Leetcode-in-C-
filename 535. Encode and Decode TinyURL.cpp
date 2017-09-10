af3696dfeed3c111f97ce3b927130878d72d1025
SkySailing <SkySailing@users.noreply.github.com>
2017-09-11 00:05:46 +0800

    Update 535. Encode and Decode TinyURL.cpp

--- 535. Encode and Decode TinyURL.cpp
+++ 535. Encode and Decode TinyURL.cpp
@@ -1,22 +1,23 @@
 public:
     string dict = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     int id = 0;
-    unordered_map<string,string> m;  //key is longURL, value is shortURL
-    unordered_map<int, string> idm;  //key is id in DB, value is longURL
+    unordered_map<string,string> m; //key is longURL, value is shortURL   //
+    unordered_map<int, string> idm; //key is id in DB, value is longURL  
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        if(m.find(longUrl) != m.end())return m[longUrl];
+        if(m.find(longUrl) != m.end())return m[longUrl];     //找 建longUrl的值，有的话，就返回对应短地址
+                         
         string res = "";
         id++;
         int count = id;
-        while(count > 0)
+        while(count > 0)     
         {
-            res = dict[count%62] + res;
-            count /= 62;
+            res = dict[count%62] + res;     //asc中，数字零对应的值为48,串联字符串,不断往后面添加字母。
+            count /= 62;                    
         }
         while(res.size() < 6)
         {
-            res = "0" + res;
+            res = "0" + res;         //不够6位时，用0添在后面。
         }
         m[longUrl] = res;
         idm[id] = longUrl;
